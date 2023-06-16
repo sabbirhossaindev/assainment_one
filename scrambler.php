1f155806f6fd16db158fce2dd71b9ebc8fbf7543
sabbir hossain <mdsabbir477470@gmail.com>
2023-06-16 19:47:09 +0600

    scrambler php done

--- scrambler.php
+++ scrambler.php
@@ -12,7 +12,7 @@ $key = 'abcdefghijklmnopqustuvwxyz1234567890';
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
-    <title>Data Scrambler</title>
+    <title>Data Scrambler By Sabbir</title>
 </head>
 <style>
     body{
